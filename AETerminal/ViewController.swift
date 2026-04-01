a076d22de6eb1d2aee63f75338cda973b6bd0e4c
tianjunqi <tianjunqi@xiaomi.com>
2026-04-01 13:29:18 +0800

    Initial Commit

---
+++ AETerminal/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AETerminal
+//
+//  Created by 田峻岐 on 2026/4/1.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ AETerminalUITests/AETerminalUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AETerminalUITestsLaunchTests.swift
+//  AETerminalUITests
+//
+//  Created by 田峻岐 on 2026/4/1.
+//
+
+import XCTest
+
+final class AETerminalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
